f44129b7c36493da1af655758aac86ef8e1ffa45
ChristopherArias-Hash <christopher.arias855@myci.csuci.edu>
2024-05-04 20:34:01 -0700

    Fixed display bug and got rid of old website features

--- frontend/src/ContactApp.jsx
+++ frontend/src/ContactApp.jsx
@@ -18,7 +18,7 @@ function ContactApp() {
 
   // Fetch contacts data from the server
   const fetchContacts = async () => {
-    const response = await fetch("http://127.0.0.1:5173/contacts");
+    const response = await fetch("http://127.0.0.1:5000/contacts");
     const data = await response.json();
     setContacts(data.contacts);
   };
